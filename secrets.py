5d5c301d988701f1d453646e17c3833863e3c1eb
Blaqsk8 <sonofstilgar@gmail.com>
2021-05-25 13:29:12 -0400

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -2,7 +2,7 @@ secrets = {
     'ssid' : 'my_ssid',
     'password' : 'my_password',
     'mqtt_broker' : '192.168.1.4',
-    'mqtt_port' : 8123,
+    'mqtt_port' : 1883,
     'mqtt_username' : 'user_name',
     'mqtt_password' : 'pass_word',
 }
