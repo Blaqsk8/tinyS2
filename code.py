eac59edb06e0b8f9e9c3c3f73f83f6d4a5373657
Blaqsk8 <sonofstilgar@gmail.com>
2021-05-25 10:28:43 -0400

    Update code.py

--- code.py
+++ code.py
@@ -9,10 +9,16 @@ import wifi
 import adafruit_minimqtt.adafruit_minimqtt as MQTT
 from adafruit_seesaw.seesaw import Seesaw
 
+# MQTT Topic Setup
+PUBLISH_DELAY = 60
+MQTT_TOPIC = "state/moisture_node_01"
+USE_DEEP_SLEEP = True
+
 # moisture sensor setup
 i2c_bus = board.I2C()
 moisture_sensor = Seesaw(i2c_bus, addr=0x36)
 
+# import secrets to initalize sensitive services
 try:
     from secrets import secrets
 except ImportError:
@@ -22,5 +28,22 @@ except ImportError:
 wifi.radio.connect(secrets["ssid"], secrets["password"])
 print("Connected to %s!" % secrets["ssid"])
 
+# create a socket pool
 pool = socketpool.SocketPool(wifi.radio)
+
+# Set up a MiniMQTT Client
+mqtt_client = MQTT.MQTT(
+    broker=secrets["mqtt_broker"],
+    port=secrets["mqtt_port"],
+    username=secrets["mqtt_username"],
+    password=secrets["mqtt_password"],
+    socket_pool=pool,
+    ssl_context=ssl.create_default_context(),
+)
+
+print("Attempting to connect to %s" % mqtt_client.broker)
+mqtt_client.connect()
+
+
+    
 
